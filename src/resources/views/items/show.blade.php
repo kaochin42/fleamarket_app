fed73470348e30499055cebaa17cfee1fe0b9faf
kaochin <kzk4289@gmail.com>
2026-05-11 15:43:44 +0900

    show.index、土台

--- src/resources/views/items/show.blade.php
+++ src/resources/views/items/show.blade.php
@@ -8,15 +8,43 @@
 
 <div>
     {{-- 商品名 --}}
-    <h2>●●●</h2>
+    <h2>{{ $item->name }}</h2>
 
     {{-- ブランド名 --}}
-    <p>●●●</p>
+    <p>{{ $item->brand_name }}</p>
 
     {{-- 価格 --}}
-    <p>¥{{ ●●● }}</p>
+    <p>¥{{ $item->price }}(税込)</p>
 
     {{-- 購入ボタン --}}
-    <a href="●●●">購入手続きへ</a>
+    <a href="{{ route('purchase.index', ['item_id' => $item->id]) }}">購入手続きへ</a>
+
+    {{-- 商品説明 --}}
+    <h3>商品説明</h3>
+    <p>{{ $item->description }}</p>
+
+    {{-- 商品の情報 --}}
+    <h3>商品の情報</h3>
+    <dl>
+        <dt>カテゴリー</dt>
+        <dd>
+            @foreach($item->categories as $category)
+            <span>{{ $category->name }}</span>
+            @endforeach
+        </dd>
+        <dt>商品の状態</dt>
+        <dd>{{ $item->condition->name }}</dd>
+    </dl>
+
+    {{-- コメント一覧 --}}
+    <h3>コメント({{ $item->comments->count() }})</h3>
+    @foreach($item->comments as $comment)
+    <div>
+        <p>{{ $comment->user->name }}</p>
+        <p>{{ $comment->comment }}</p>
+    </div>
+    @endforeach
+
+    {{-- コメントフォーム（後で実装） --}}
 </div>
 @endsection
