e5a1696b7428f79ae94b6dd38e5ce410e984461e
kaochin <kzk4289@gmail.com>
2026-05-23 13:21:39 +0900

    共通css

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -4,23 +4,30 @@
 <head>
     <meta charset="UTF-8">
     <title>flea market</title>
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <header>
-        <h1><a href="{{ route('item.index') }}">COACHTECH</a></h1>
-        <input type="text" placeholder="なにをお探しですか？">
-        @auth
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
-            <button type="submit">ログアウト</button>
-        </form>
-        <a href="{{ route('mypage.show') }}">マイページ</a>
-        <a href="{{ route('item.create') }}">出品</a>
-        @else
-        <a href="{{ route('login') }}">ログイン</a>
-        <a href="{{ route('register') }}">会員登録</a>
-        @endauth
+    <header class="header">
+        <h1 class="header-logo">
+            <a href="{{ route('item.index') }}"><img class="header-logo-img" src="{{ asset('images/COACHTECHヘッダーロゴ.png') }}" alt="COACHTECH"></a>
+        </h1>
+        <input class="header-search" type="text" placeholder="なにをお探しですか？">
+        <nav class="header-nav">
+            @auth
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <button class="nav-button" type="submit">ログアウト</button>
+            </form>
+            <a class="nav-link" href="{{ route('mypage.show') }}">マイページ</a>
+            <a class="nav-sell" href="{{ route('item.create') }}">出品</a>
+            @else
+            <a class="nav-link" href="{{ route('login') }}">ログイン</a>
+            <a class="nav-link" href="{{ route('register') }}">会員登録</a>
+            @endauth
+        </nav>
     </header>
 
     <main>
--- src/resources/views/layouts/auth.blade.php
+++ src/resources/views/layouts/auth.blade.php
@@ -4,11 +4,16 @@
 <head>
     <meta charset="UTF-8">
     <title>flea market</title>
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <header>
-        <h1><a href="{{ route('item.index') }}">COACHTECH</a></h1>
+    <header class="header">
+        <h1 class="header-logo">
+            <a href="{{ route('item.index') }}"><img class="header-logo-img" src="{{ asset('images/COACHTECHヘッダーロゴ.png') }}" alt="COACHTECH"></a>
+        </h1>
     </header>
 
     <main>
