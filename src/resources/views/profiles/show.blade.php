23f6ec612e1a13f1a2e181bb7a4f7b9b8764d720
kaochin <kzk4289@gmail.com>
2026-05-24 18:46:49 +0900

    マイページcss

--- src/resources/views/profiles/show.blade.php
+++ src/resources/views/profiles/show.blade.php
@@ -1,34 +1,45 @@
 @extends('layouts.app')
+@push('styles')
+<link rel="stylesheet" href="{{ asset('css/profile.css') }}">
+@endpush
 @section('content')
 
 {{-- プロフィール情報 --}}
-<div>
-    <img src="{{ asset('storage/' . $user->profile_img) }}" alt="プロフィール画像">
-    <p>{{ $user->name }}</p>
-    <a href="{{ route('profile.edit') }}">プロフィールを編集</a>
+<div class="user-info">
+    <img class="profile-img" src="{{ asset('storage/' . $user->profile_img) }}" alt="プロフィール画像">
+    <p class="profile-name">{{ $user->name }}</p>
+    <a class="action-bar-outline" href="{{ route('profile.edit') }}">プロフィールを編集</a>
 </div>
 
 {{-- タブ --}}
-<div>
-    <a href="{{ route('mypage.show') }}?page=sell">出品した商品</a>
-    <a href="{{ route('mypage.show') }}?page=buy">購入した商品</a>
+<div class="tab-outer">
+    <div class="tab-area">
+        <a class="tab-link {{ request('page') !== 'buy' ? 'tab-active' : '' }}" href="{{ route('mypage.show') }}?page=sell">出品した商品</a>
+        <a class="tab-link {{ request('page') === 'buy' ? 'tab-active' : '' }}" href="{{ route('mypage.show') }}?page=buy">購入した商品</a>
+    </div>
 </div>
 
 {{-- 商品一覧 --}}
-@if(request('page') == 'buy')
-@foreach($buyItems as $item)
-<div>
-    <img src="{{ $item->image_path }}" alt="{{ $item->name }}">
-    <p>{{ $item->name }}</p>
+<div class="products-row">
+    @if(request('page') == 'buy')
+    @foreach($buyItems as $item)
+    <a class="product-card" href="{{ route('item.show', ['item_id' => $item->id]) }}">
+        <div class="product-img-wrap">
+            <img class="product-img" src="{{ Str::startsWith($item->image_path, 'http') ? $item->image_path : asset('storage/' . $item->image_path) }}" alt="{{ $item->name }}">
+        </div>
+        <p class="product-name">{{ $item->name }}</p>
+    </a>
+    @endforeach
+    @else
+    @foreach($sellItems as $item)
+    <a class="product-card" href="{{ route('item.show', ['item_id' => $item->id]) }}">
+        <div class="product-img-wrap">
+            <img class="product-img" src="{{ Str::startsWith($item->image_path, 'http') ? $item->image_path : asset('storage/' . $item->image_path) }}" alt="{{ $item->name }}">
+        </div>
+        <p class="product-name">{{ $item->name }}</p>
+    </a>
+    @endforeach
+    @endif
 </div>
-@endforeach
-@else
-@foreach($sellItems as $item)
-<div>
-    <img src="{{ $item->image_path }}" alt="{{ $item->name }}">
-    <p>{{ $item->name }}</p>
-</div>
-@endforeach
-@endif
 
 @endsection
