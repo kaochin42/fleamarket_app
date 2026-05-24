21991c8c0593ef92495d46e846821ba4a00be3f0
kaochin <kzk4289@gmail.com>
2026-05-24 13:31:00 +0900

    商品一覧css（画像保存、seeder再施工前）

--- src/resources/views/items/index.blade.php
+++ src/resources/views/items/index.blade.php
@@ -1,13 +1,27 @@
 @extends('layouts.app')
+@push('styles')
+<link rel="stylesheet" href="{{ asset('css/items.css') }}">
+@endpush
 
 @section('content')
-@foreach($items as $item)
-<a href="{{ route('item.show', ['item_id' => $item->id]) }}">
-    <img src="{{ asset('storage/' . $item->image_path) }}" alt="{{ $item->name }}">
-    @if($purchasedItemIds->contains($item->id))
-        <p>sold</p>
-    @endif
-    <p>{{ $item->name }}</p>
-</a>
-@endforeach
+<div class="tab-area">
+    <a class="tab-link" href="/">おすすめ</a>
+    <a class="tab-link" href="/?tab=mylist">マイリスト</a>
+</div>
+
+<div class="products-row">
+    @foreach($items as $item)
+    <a class="product-card" href="{{ route('item.show', ['item_id' => $item->id]) }}">
+        <div class="product-img-wrap">
+            <img class="product-img" src="{{ asset('storage/' . $item->image_path) }}" alt="{{ $item->name }}">
+            @if($purchasedItemIds->contains($item->id))
+            <div class="sold-overlay">
+                <p class="sold-text">sold</p>
+            </div>
+            @endif
+        </div>
+        <p class="product-name">{{ $item->name }}</p>
+    </a>
+    @endforeach
+</div>
 @endsection
