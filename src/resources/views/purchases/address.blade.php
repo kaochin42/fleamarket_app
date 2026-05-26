cd1955883f5c31ebda89f23608f9c8a1bf2d85ad
kaochin <kzk4289@gmail.com>
2026-05-26 12:33:13 +0900

    住所変更css

--- src/resources/views/purchases/address.blade.php
+++ src/resources/views/purchases/address.blade.php
@@ -1,34 +1,35 @@
 @extends('layouts.app')
+@push('styles')
+<link rel="stylesheet" href="{{ asset('css/profile.css') }}">
+<link rel="stylesheet" href="{{ asset('css/address.css') }}">
+@endpush
 
 @section('content')
-<h2>住所の変更</h2>
+<div class="inner">
+    <h2 class="title">住所の変更</h2>
 
-@if($errors->any())
-<ul>
-    @foreach($errors->all() as $error)
-    <li>{{ $error }}</li>
-    @endforeach
-</ul>
-@endif
+    <form method="POST" action="{{ route('address.update', ['item_id' => $item->id]) }}">
+        @csrf
 
-<form method="POST" action="{{ route('address.update', ['item_id' => $item->id]) }}">
-    @csrf
+        <div class="form-group form-group-postcode">
+            <label class="label">郵便番号</label>
+            <input class="input" type="text" name="postcode" value="{{ old('postcode', $user->postcode) }}">
+            @error('postcode')<p class="error">{{ $message }}</p>@enderror
+        </div>
 
-    <div>
-        <label>郵便番号</label>
-        <input type="text" name="postcode" value="{{ $user->postcode }}">
-    </div>
+        <div class="form-group form-group-address">
+            <label class="label">住所</label>
+            <input class="input" type="text" name="address" value="{{ old('address', $user->address) }}">
+            @error('address')<p class="error">{{ $message }}</p>@enderror
+        </div>
 
-    <div>
-        <label>住所</label>
-        <input type="text" name="address" value="{{ $user->address }}">
-    </div>
+        <div class="form-group form-group-building">
+            <label class="label">建物名</label>
+            <input class="input" type="text" name="building" value="{{ old('building', $user->building) }}">
+            @error('building')<p class="error">{{ $message }}</p>@enderror
+        </div>
 
-    <div>
-        <label>建物名</label>
-        <input type="text" name="building" value="{{ $user->building }}">
-    </div>
-
-    <button type="submit">更新する</button>
-</form>
+        <button class="action-bar" type="submit">更新する</button>
+    </form>
+</div>
 @endsection
